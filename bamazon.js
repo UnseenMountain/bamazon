60a3d971621a8323e0c335d502099b029dcce835
Luke Klymshyn <unseenmountain@gmail.com>
2020-01-11 14:06:24 -0800

    added json files and mysql

--- bamazon.js
+++ bamazon.js
@@ -1,26 +1,51 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "root",
-  database: "bamazonDB"
+    // Your password
+    password: "root",
+    database: "bamazonDB"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  populateList();
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    populateList();
+    selectProduct();
 });
 
 function populateList() {
-    inquire.prompt
+
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "input",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(res[i].id + " | " + res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
+                        }
+                        return choiceArray;
+                    },
+                    message: "Please select an item id?"
+                }
+            ])
+
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + " | " + res[i].item_id + " | " + res[i].product_name + " | " + res[i].price)
+        }
+    })
+
 }
 
